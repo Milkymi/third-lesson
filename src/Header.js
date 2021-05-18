11bf3cc7d3603d44f293f4cc6007011d9cb9f3e9
milka <milka@mail.com>
2021-05-18 13:59:02 +0300

    first commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from "./Logo";
+import Menu from "./Menu";
+function Header(props) {
+    return (
+        <div className={"dis-header"}>
+            <Logo />
+            <Menu />
+        </div>
+    );
+}
+
+export default Header;
